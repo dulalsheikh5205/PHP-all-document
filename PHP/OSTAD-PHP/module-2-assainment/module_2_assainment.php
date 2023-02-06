af31b412b454524e7c2a4960623d8fd1e51bab43
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-02-06 19:30:37 +0600

    update the operation

--- PHP/OSTAD-PHP/module-2-assainment/module_2_assainment.php
+++ PHP/OSTAD-PHP/module-2-assainment/module_2_assainment.php
@@ -2,13 +2,13 @@
  $tutionFee = 21000;
 
 /* 
- if($tutionFee > 20000){
+ if($tutionFee >= 20000){
     echo "commission is twenty-five percent is = " .(25/100)*$tutionFee." $";
  }
- else if($tutionFee > 10000 && $tutionFee < 20000){
+ else if($tutionFee >= 10000 && $tutionFee < 20000){
     echo "commission is twenty percent = " .(20/100)*$tutionFee." $";
  }
- else if($tutionFee < 10000 && $tutionFee > 7000){
+ else if($tutionFee < 10000 && $tutionFee >= 7000){
     echo"commission rate is fifteen percent = ".(15/100)*$tutionFee." $";
  }
  else if( $tutionFee < 7000){
@@ -16,9 +16,9 @@
  }
  */
 
- $result = $tutionFee > 20000 ? "commission is twenty-five percent is = " .(25/100)*$tutionFee." $":
- ($tutionFee > 10000 && $tutionFee < 20000 ? "commission is twenty percent = " .(20/100)*$tutionFee." $":
- ($tutionFee < 10000 && $tutionFee > 7000 ? "commission rate is fifteen percent = ".(15/100)*$tutionFee." $":
+ $result = $tutionFee >= 20000 ? "commission is twenty-five percent is = " .(25/100)*$tutionFee." $":
+ ($tutionFee >= 10000 && $tutionFee < 20000 ? "commission is twenty percent = " .(20/100)*$tutionFee." $":
+ ($tutionFee < 10000 && $tutionFee >= 7000 ? "commission rate is fifteen percent = ".(15/100)*$tutionFee." $":
    ($tutionFee < 7000 ? "the data will be invalid":"")));
    
  echo $result;
